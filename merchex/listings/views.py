32d35d7fdf592978e79c626cbeaf1ea3393f4aa8
Salah-Eddine ET-TALEBY <salah_ettaleby@icloud.com>
2024-01-05 14:16:33 +0100

    Added: Multiple files (MOOC in progress, exercises, ...)

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,25 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from listings.models import Band
+from listings.models import Band, Listing
 
 
 def hello(request):
     bands = Band.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello Django !</h1>
-        <p>Mes groupes préférés sont :<p>
-        <ul>
-            <li>{bands[0].name}</li>
-            <li>{bands[1].name}</li>
-            <li>{bands[2].name}</li>
-        </ul>
-""")
+    return render(request, 'listings/hello.html', {'bands': bands})
 
 def about(request):
-    return HttpResponse("<h1>À propos</h1> <p>Nous adorons merch !</p>")
-
+    return render(request, 'listings/about-us.html')
 def listings(request):
-    return HttpResponse("<h1>Nos annonces</h1> <ul><li>Annonce 1</li><li>Annonce 2</li><li>Annonce 3</li></ul>")
+    listings = Listing.objects.all()
+    return render(request, 'listings/listings.html', {'listings': listings})
 
 def contact(request):
-    return HttpResponse("<h1>Contactez-nous !</h1> <p>Envoyez-nous un e-mail à l'adresse suivante : x@y.z")
+    return render(request, 'listings/contact-us.html')
